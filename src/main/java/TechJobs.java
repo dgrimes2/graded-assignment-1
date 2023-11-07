626e031a964983111d81c547163bec978a24c4d6
dgrimes2 <grime22d@mtholyoke.edu>
2023-11-06 20:33:45 -0600

    commit -fixed failed tests

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -122,19 +122,20 @@ public class TechJobs {
 //      Need to iterate through an arraylist of hashmaps
 //      Use Map.Entry<> the nested iteration (hashmaps)
         if (someJobs.size() == 0) {
-            System.out.println("No Results");
+            System.out.print("No Results");
 //            figure out how to eliminate the space underneath here
         } else {
 //          loop through the arraylist first (to access a hashmap at a time
             for (int i = 0; i < someJobs.size(); i++) {
-                System.out.println("*****");
+                System.out.println("\n*****");
 //              now loop through each key/value entry of the selected hashmap
                 for (Map.Entry<String, String> entry : someJobs.get(i).entrySet()) {
 //                  print out key: value of the selected hashmap
 
                     System.out.println(entry.getKey() +": " + entry.getValue());
+
                 }
-                System.out.println("*****\n");
+                System.out.println("*****");
 //                System.out.println("\n");
             }
         }
